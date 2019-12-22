c9cf7d49bf21794bf024605b33220125de6eca8d
nnick <nnickspr@gmail.com>
2019-12-22 14:07:41 +0300

    add empty project

---
+++ Map/Map/Map/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by Николай Спиридонов on 22.12.2019.
+//  Copyright © 2019 nnick. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
